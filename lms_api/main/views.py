713c7a1b7b97ba48d6461d1d9baabe2947ad4a1a
Rupert Garcia <rupert.garcia.ph@gmail.com>
2025-05-13 10:21:43 +0800

    Generic Views

--- lms_api/main/views.py
+++ lms_api/main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 from .serializers import TeacherSerializer
 from . import models
 
-class TeacherList(APIView):
-    def get(self,request):
-        teachers=models.Teacher.objects.all()
-        serializer = TeacherSerializer(teachers, many=True)
-        return Response(serializer.data)
+class TeacherList(generics.ListCreateAPIView):
+    queryset=models.Teacher.objects.all()
+    serializer_class=TeacherSerializer
+
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=models.Teacher.objects.all()
+    serializer_class=TeacherSerializer
